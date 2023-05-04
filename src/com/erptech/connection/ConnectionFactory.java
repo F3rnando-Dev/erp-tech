65ea7c7074a4790d524147fe115475094428c977
Fernando Santana <fernando_92-@hotmail.com>
2023-05-03 23:17:59 -0300

    feat: add connection factory do projeto

---
+++ src/com/erptech/connection/ConnectionFactory.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.erptech.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author ferna
+ */
+public class ConnectionFactory {
+    private final String DRIVER = "com.mysql.cj.jdbc.Drive";
+    private final String URL = "jdbc:mysql://localhost:3306/erptech?characterEncoding=utf8";
+    private final String USER = "root";
+    private final String PASSWORD = "root";
+    
+    public Connection getConnection(){
+        try{
+            Class.forName(DRIVER);
+            
+            Connection conn = DriverManager.getConnection(URL, USER,PASSWORD);
+            return conn;
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("Erro na conexão "+ex);
+        }
+    }
+    
+    public static void closeConnection(Connection conn){
+        try{
+            if(conn != null){
+                conn.close();
+            }
+        } catch (SQLException ex){
+            throw new RuntimeException("Problem na exception "+ex);
+        }
+    }
+    
+    public static void closeConnection(Connection conn, PreparedStatement stmt){
+        closeConnection(conn);
+        
+        try{
+            if(stmt != null){
+            stmt.close();
+        }   
+        } catch (SQLException ex){
+            throw new RuntimeException("Problem na exception "+ex);
+        }
+    }
+    
+    public static void closeConnection(Connection conn, PreparedStatement stmt, ResultSet rs){
+        
+        closeConnection(conn, stmt);
+        
+        try{
+            if(rs != null){
+            rs.close();
+        }   
+        } catch (SQLException ex){
+            throw new RuntimeException("Problem na exception "+ex);
+        }
+    }
+    
+    
+    
+    
+}
