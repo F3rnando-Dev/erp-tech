76906e01626daaaf1450768b700b3f685be7c7d4
Fernando Santana <fernando_92-@hotmail.com>
2023-05-04 00:10:50 -0300

    feat: add metodo creat na dao para salvar funcionario no banco

--- src/com/erptech/dao/CadastroFuncionarioDao.java
+++ src/com/erptech/dao/CadastroFuncionarioDao.java
@@ -4,10 +4,43 @@
  */
 package com.erptech.dao;
 
+import com.erptech.connection.ConnectionFactory;
+import com.erptech.model.CadastroFuncionarioModel;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ferna
  */
 public class CadastroFuncionarioDao {
+    
+    
+    public void create(CadastroFuncionarioModel funcionario){
+        Connection conn = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+            stmt = conn.prepareStatement("INSERT INTO cadastro_funcionario (matricula_funcionario, nome_funcionario, cargo_funcionario) VALUES (?, ?, ?)");
+            stmt.setString(1, funcionario.getMatriculaDoFuncionario());
+            stmt.setString(2, funcionario.getNomeDoFuncionario());
+            stmt.setString(3, funcionario.getCargoDoFuncionario());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null,"Funcionario salvo com sucesso!");
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Erro ao salvar funcionário!\n"+ex);
+        } finally{
+            ConnectionFactory.closeConnection(conn,stmt);
+            
+            
+        }
+    }
     
 }
